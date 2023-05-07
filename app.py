ef8d700686a1a2eaecf4aabe80cf36ae109c5b92
Anastasia Iva <83039057+anaaaiva@users.noreply.github.com>
2023-05-07 11:32:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import gradio as gr
 from functions import *
 
 title = "Water Body Segmentation from satellite (aerial) images - PyTorch"
-examples = ['examples/example1.png',
-            'examples/example2.png',
-            'examples/example3.png',
-            'examples/example4.png',
-            'examples/example5.png']
+examples = ['examples/example1.jpg',
+            'examples/example2.jpg',
+            'examples/example3.jpg',
+            'examples/example4.jpg',
+            'examples/example5.jpg']
 
 interface = gr.Interface(fn=predict, inputs=gr.Image(type="numpy").style(height= 256),
             outputs=gr.Image(type="numpy").style(height=256),
